ab81259faae1413042e88e16ce4f55596fc2aa6c
Roman Rabodzei <roman.rabodzei@icloud.com>
2024-07-10 08:51:36 -0500

    added container apps env tf template

---
+++ terraform/resources/containerApps/main.tf
@@ -0,0 +1,11 @@
+/*
+.Synopsis
+    Terraform template for Container Apps.
+    Template:
+      - https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/
+      - https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/
+
+.NOTES
+    Author     : Roman Rabodzei
+    Version    : 1.0.240710
+*/
